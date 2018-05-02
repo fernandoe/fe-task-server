846e29c9ea6a6af448e3eacb5bb27ed1dd60eb22
Fernando Espíndola <fer.esp@gmail.com>
2018-05-02 11:56:35 -0300

    Add task model

--- src/fe_task/models.py
+++ src/fe_task/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from fe_core.models import UUIDModel, Entity
 
-# Create your models here.
+User = get_user_model()
+
+
+class Task(UUIDModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    entity = models.ForeignKey(Entity, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
